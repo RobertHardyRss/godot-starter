a7b3ef6c22d650f7394751dafbe026b1ac19ae87
Robert Hardy <rhardy@dcsdk12.org>
2026-04-15 15:30:53 -0600

    Setting up animation tree for player

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,3 +1,4 @@
+class_name Player
 extends CharacterBody2D
 
 @export var move_speed := 220.0
@@ -8,6 +9,15 @@ extends CharacterBody2D
 @export var world_bounds_right: CollisionShape2D
 @export var world_bounds_bottom: CollisionShape2D
 @export var world_bounds_left: CollisionShape2D
+
+enum PlayerStates {
+	IDLE,
+	WALKING,
+	FALLING,
+	JUMPING,
+}
+
+var current_state: PlayerStates = PlayerStates.IDLE
 
 func _ready():
 	set_camera_limits()
@@ -22,15 +32,16 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 
+	# determine current state
+	if not is_on_floor():
+		current_state = PlayerStates.FALLING if velocity.y > 0 else PlayerStates.JUMPING
+	elif velocity.x != 0:
+		current_state = PlayerStates.WALKING
+	else:
+		current_state = PlayerStates.IDLE
+	
 	move_and_slide()
 
-#func set_camera_boundaries(top: Vector2, right: Vector2, bottom: Vector2, left: Vector2) -> void:
-	#var camera := %Camera2D
-	#
-	#camera.limit_bottom = bottom.y
-	#camera.limit_left = left.x
-	#camera.limit_top = top.y
-	#camera.limit_right = right.x
 	
 func set_camera_limits() -> void:
 	var camera := %Camera2D
