49059f9719948b0762e8fd06bb557cb1087d2d98
Robert Hardy <rhardy@dcsdk12.org>
2026-04-15 16:26:39 -0600

    Finish up player animations

---
+++ scenes/player/player_animations.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@export var anim_tree: AnimationTree
+@onready var player: Player = get_owner()
+
+var facing = 0
+
+func _physics_process(_delta) -> void:
+	var state = player.current_state
+	
+	if player.velocity.x != 0:
+		facing = player.velocity.x
+	
+	# setup conditions based on current state
+	anim_tree.set("parameters/conditions/Falling", state == Player.PlayerStates.FALLING)
+	anim_tree.set("parameters/conditions/Idle", state == Player.PlayerStates.IDLE)
+	anim_tree.set("parameters/conditions/Jumping", state == Player.PlayerStates.JUMPING)
+	anim_tree.set("parameters/conditions/Walking", state == Player.PlayerStates.WALKING)
+
+	# set blend to player velocity x
+	anim_tree.set("parameters/Falling/blend_position", facing)
+	anim_tree.set("parameters/Idle/blend_position", facing)
+	anim_tree.set("parameters/Jumping/blend_position", facing)
+	anim_tree.set("parameters/Walking/blend_position", facing)
