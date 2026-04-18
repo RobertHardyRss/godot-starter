635436628b3e694e652b2240bdad8aa0ee3ee790
Robert Hardy <rhardy@dcsdk12.org>
2026-04-18 15:49:07 -0600

    Finish coin collection

--- scenes/items/coin.gd
+++ scenes/items/coin.gd
@@ -6,8 +6,4 @@ func _on_body_entered(body: Node) -> void:
 	print("coin entered")
 	if body.is_in_group("player"):
 		collected.emit()
-		call_deferred("play_sound")
-		queue_free()
-
-func play_sound() -> void:
-	%CollectSound.play()
+		%AnimationPlayer.play("collect")
