64ed35e375249e6122a27cacbab113d4ff56ec70
Robert Hardy <rhardy@dcsdk12.org>
2026-05-12 15:51:41 -0600

    Adjust collision layers and masks

--- scenes/items/coin.gd
+++ scenes/items/coin.gd
@@ -3,7 +3,6 @@ extends Area2D
 signal collected
 
 func _on_body_entered(body: Node) -> void:
-	print("coin entered")
 	if body.is_in_group("player"):
 		collected.emit()
 		%AnimationPlayer.play("collect")
